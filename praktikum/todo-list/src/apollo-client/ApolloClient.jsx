b084665d719a4cb3fb98ede25391ed10b10492e8
DargonFly378 <hafichever@gmail.com>
2022-05-07 12:12:35 +0700

    update env to variabel biasa

--- praktikum/todo-list/src/apollo-client/ApolloClient.jsx
+++ praktikum/todo-list/src/apollo-client/ApolloClient.jsx
@@ -1,10 +1,19 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+// const client = new ApolloClient({
+//   uri: process.env.REACT_APP_SERVER_GRAPHQL,
+//   cache: new InMemoryCache(),
+//   headers: {
+//     "x-hasura-admin-secret": process.env.REACT_APP_SECRET_KEY,
+//   },
+// });
+
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_SERVER_GRAPHQL,
+  uri: "https://todolist-hafiz.hasura.app/v1/graphql",
   cache: new InMemoryCache(),
   headers: {
-    "x-hasura-admin-secret": process.env.REACT_APP_SECRET_KEY,
+    "x-hasura-admin-secret":
+      "pW2HjOlCnFz7eAAOdZAt57SmA1ZKJukrhg9sKaYUd8C10NY4BlpAg6B54QbX6Atv",
   },
 });
 export default client;
